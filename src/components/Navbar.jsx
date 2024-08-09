af2cef2479fa7f3ab2098a267fb7faec686001b3
kabirasunny <sunnykabira5507@gmail.com>
2024-08-09 16:48:22 +0530

    I have added section for logo in Navbar.jsx

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Navbar.css'
+
+const Navbar = () => {
+  return (
+    <>
+      <section className="headLogo">
+        <div className="line"><div></div></div>
+        <div className="line secondLine"><div></div></div>
+        <div className="logo"><span>The Pub</span></div>
+        <div className="line secondLine"><div></div></div>
+        <div className="line"><div></div></div>
+        <div className='logoPara'><p>The best night in town</p></div>
+      </section>
+    </>
+  )
+}
+
+export default Navbar
