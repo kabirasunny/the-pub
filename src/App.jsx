a81e497f6010d4a5a4da77428d201830d6576f34
kabirasunny <sunnykabira5507@gmail.com>
2024-08-11 13:07:43 +0530

    I deleted a old video

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import viteLogo from '/vite.svg'
 // import './App.css'
 import Navbar from './components/Navbar'
 import Home from './components/Home'
+import Footer from './components/Footer'
 
 function App() {
 
@@ -12,6 +13,7 @@ function App() {
     <>
     <Navbar/>
     <Home/>
+    <Footer/>
     </>
   )
 }
