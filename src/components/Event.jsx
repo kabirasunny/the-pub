46b65cd50a4e27d403c636e67093f8e7761b9775
kabirasunny <sunnykabira5507@gmail.com>
2024-08-17 22:27:24 +0530

    Now i added new file for events and a section in Event.jsx

---
+++ src/components/Event.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Event.css'
+
+const Event = () => {
+    return (
+        <>
+            <section className="aboutEvent">
+                <div className="aeDiv">
+                    <h1 className="aeTitle">Events</h1>
+                    <p className="aePara">I'm a paragraph. Click here to add your own text and edit
+                        me. It’s easy. Just click “Edit Text” or double click me and you can start adding your
+                        own content and make changes to the font. I’m a great place for you to let your users know
+                        a little more about you. If you want to delete me just click on me and press delete.​</p>
+                </div>
+            </section>
+        </>
+    )
+}
+
+export default Event
