55d717c8595554c755a9b8e3feef2c59ac450036
kabirasunny <sunnykabira5507@gmail.com>
2024-09-03 14:26:21 +0530

    I have created now 'getUser function for GET user details from url to backend'

--- src/services/user-service.js
+++ src/services/user-service.js
@@ -3,3 +3,8 @@ import { myAxios } from "./helper";
 export const signUp = (user) => {
     return myAxios.post('/users/register', user).then((response) => response.data)
 }
+
+export const getUser = (number,user) => {
+    const num = parseInt(number)
+    return myAxios.get(`/users/getData/${num}`,user).then((response) => response.data)
+}
