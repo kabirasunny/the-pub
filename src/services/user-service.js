5975986e9144f12f4ba5aa608ec0654d361e9e47
kabirasunny <sunnykabira5507@gmail.com>
2024-09-13 17:46:00 +0530

    I have added async and await for fetch data

--- src/services/user-service.js
+++ src/services/user-service.js
@@ -1,15 +1,18 @@
+import { useEffect } from "react";
 import { myAxios } from "./helper";
 
 export const signUp = (user) => {
     return myAxios.post('/users/register', user).then((response) => response.data)
 }
 
-export const getUser = (number,user) => {
+export const getUser = (number, user) => {
     const num = parseInt(number)
-    return myAxios.get(`/users/getData/${num}`,user).then((response) => response.data)
+    return myAxios.get(`/users/getData/${num}`, user).then((response) => response.data)
 }
 
-export  const getCards = () =>{
-    return myAxios.get('/').then((response)=> response.data)
-   
+
+export const getCards = async () => {
+    const response =await myAxios.get('/').then((response) => response.data)
+    console.log(response);
+    return response;
 }
